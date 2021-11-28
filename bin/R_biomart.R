dafd8ee2ee36275076283d864ea5b4b4f252ba34
chriswyatt1 <chris-wyatt@hotmail.com>
2021-11-28 18:16:46 +0000

    Correct_config_Fix_R_Script

--- bin/R_biomart.R
+++ bin/R_biomart.R
@@ -5,11 +5,11 @@ ensembl_entry <- useEnsembl(biomart = (args[1]), dataset=(args[3]), mirror="uswe
 get_go_ids <- getBM(attributes = c('ensembl_gene_id','go_id'), mart = ensembl_entry)
 write.table(get_go_ids, "go_hash.txt", row.names=T, quote=F, sep="\t" )
 
-d<-unique(get_go_ids\$ensembl_gene_id)
+d<-unique(get_go_ids$ensembl_gene_id)
 new_ids<- split(d, ceiling(seq_along(d)/500))
 
 for(i in 1:length(new_ids)) {
-bazooka2 <- getBM(attributes = c('ensembl_gene_id','peptide'), mart = ensembl_apis, values=new_ids2[i], filters='ensembl_gene_id',  uniqueRows=T)
+bazooka2 <- getBM(attributes = c('ensembl_gene_id','peptide'), mart = ensembl_entry, values=new_ids[i], filters='ensembl_gene_id',  uniqueRows=T)
 Sys.sleep(runif(1, 5.0, 10))
 outname <- paste("Myoutput_", i, ".fasta", sep="")
 write.table(bazooka2, file=outname, row.names=F, quote=F, sep="\n")
