bf569412bb080ed70c95834d735aacdf0f90283e
chriswyatt1 <chris-wyatt@hotmail.com>
2021-11-29 21:32:48 +0000

    Add_more_gofiles

--- bin/R_biomart.R
+++ bin/R_biomart.R
@@ -6,7 +6,7 @@ get_go_ids <- getBM(attributes = c('ensembl_gene_id','go_id'), mart = ensembl_en
 write.table(get_go_ids, "go_hash.txt", row.names=T, quote=F, sep="\t" )
 
 d<-unique(get_go_ids$ensembl_gene_id)
-new_ids<- split(d, ceiling(seq_along(d)/500))
+new_ids<- split(d, ceiling(seq_along(d)/200))
 
 for(i in 1:length(new_ids)) {
 bazooka2 <- getBM(attributes = c('ensembl_gene_id','peptide'), mart = ensembl_entry, values=new_ids[i], filters='ensembl_gene_id',  uniqueRows=T)
